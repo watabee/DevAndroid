2821adfad27af19c2d920c770460d9dfe90b90c4
watabee <watabee.dev@gmail.com>
2019-09-17 22:37:30 +0900

    Configure dependencies

---
+++ buildSrc/src/main/java/dependencies/dependencies.kt
@@ -0,0 +1,115 @@
+package dependencies
+
+object Deps {
+
+    const val androidGradlePlugin = "com.android.tools.build:gradle:3.5.0"
+
+    const val timber = "com.jakewharton.timber:timber:4.7.1"
+
+    const val junit = "junit:junit:4.12"
+
+    object Kotlin {
+        private const val version = "1.3.50"
+        const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$version"
+        const val gradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$version"
+    }
+
+    object Google {
+        const val material = "com.google.android.material:material:1.1.0-alpha10"
+    }
+
+    object AndroidX {
+        const val appcompat = "androidx.appcompat:appcompat:1.1.0"
+        const val recyclerview = "androidx.recyclerview:recyclerview:1.1.0-beta04"
+        const val swiperefresh = "androidx.swiperefreshlayout:swiperefreshlayout:1.1.0-alpha02"
+
+        object Activity {
+            private const val version = "1.1.0-alpha03"
+            const val activityKtx = "androidx.activity:activity-ktx:$version"
+        }
+
+        object Fragment {
+            private const val version = "1.2.0-alpha03"
+            const val fragmentKtx = "androidx.fragment:fragment-ktx:$version"
+        }
+
+        object Test {
+            private const val version = "1.2.0"
+            const val core = "androidx.test:core:$version"
+            const val runner = "androidx.test:runner:$version"
+            const val rules = "androidx.test:rules:$version"
+
+            const val espressoCore = "androidx.test.espresso:espresso-core:3.2.0"
+        }
+
+        const val archCoreTesting = "androidx.arch.core:core-testing:2.1.0"
+
+        const val constraintlayout = "androidx.constraintlayout:constraintlayout:2.0.0-beta2"
+
+        const val coreKtx = "androidx.core:core-ktx:1.2.0-alpha04"
+
+        object Lifecycle {
+            private const val version = "2.2.0-alpha04"
+            const val extensions = "androidx.lifecycle:lifecycle-extensions:$version"
+            const val compiler = "androidx.lifecycle:lifecycle-compiler:$version"
+            const val viewmodel = "androidx.lifecycle:lifecycle-viewmodel-ktx:$version"
+        }
+
+        object Room {
+            private const val version = "2.2.0-rc01"
+            const val common = "androidx.room:room-common:$version"
+            const val runtime = "androidx.room:room-runtime:$version"
+            const val compiler = "androidx.room:room-compiler:$version"
+            const val ktx = "androidx.room:room-ktx:$version"
+            const val testing = "androidx.room:room-testing:$version"
+        }
+    }
+
+    object RxJava {
+        const val rxJava = "io.reactivex.rxjava2:rxjava:2.2.11"
+        const val rxAndroid = "io.reactivex.rxjava2:rxandroid:2.1.1"
+        const val rxKotlin = "io.reactivex.rxjava2:rxkotlin:2.4.0"
+    }
+
+    object Dagger {
+        private const val version = "2.24"
+        const val dagger = "com.google.dagger:dagger:$version"
+        const val androidSupport = "com.google.dagger:dagger-android-support:$version"
+        const val compiler = "com.google.dagger:dagger-compiler:$version"
+        const val androidProcessor = "com.google.dagger:dagger-android-processor:$version"
+    }
+
+    object Glide {
+        private const val version = "4.9.0"
+        const val glide = "com.github.bumptech.glide:glide:$version"
+        const val compiler = "com.github.bumptech.glide:compiler:$version"
+    }
+
+    object OkHttp {
+        private const val version = "4.1.0"
+        const val okhttp = "com.squareup.okhttp3:okhttp:$version"
+        const val loggingInterceptor = "com.squareup.okhttp3:logging-interceptor:$version"
+    }
+
+    object Retrofit {
+        private const val version = "2.6.1"
+        const val retrofit = "com.squareup.retrofit2:retrofit:$version"
+        const val rxJavaAdapter = "com.squareup.retrofit2:adapter-rxjava2:$version"
+        const val moshiConverter = "com.squareup.retrofit2:converter-moshi:$version"
+    }
+
+    object Moshi {
+        private const val version = "1.8.0"
+        const val kotlin = "com.squareup.moshi:moshi-kotlin:$version"
+        const val codegen = "com.squareup.moshi:moshi-kotlin-codegen:$version"
+    }
+
+    object AssistedInject {
+        private const val version = "0.5.0"
+        const val annotationDagger2 =
+            "com.squareup.inject:assisted-inject-annotations-dagger2:$version"
+        const val processorDagger2 =
+            "com.squareup.inject:assisted-inject-processor-dagger2:$version"
+    }
+}
+

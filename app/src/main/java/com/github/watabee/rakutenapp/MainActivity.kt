0cbd538f85ce3c8edd9b7f8334413f98309acbea
watabee <watabee.dev@gmail.com>
2019-10-14 19:07:59 +0900

    Create base activity

--- app/src/main/java/com/github/watabee/rakutenapp/MainActivity.kt
+++ app/src/main/java/com/github/watabee/rakutenapp/MainActivity.kt
@@ -1,21 +1,17 @@
 package com.github.watabee.rakutenapp
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import com.github.watabee.rakutenapp.di.ActivityScope
 import com.github.watabee.rakutenapp.util.Logger
 import dagger.Module
-import dagger.android.AndroidInjection
 import dagger.android.ContributesAndroidInjector
 import javax.inject.Inject
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
     @Inject lateinit var logger: Logger
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        AndroidInjection.inject(this)
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
---
+++ ui/common/src/main/java/com/github/watabee/rakutenapp/BaseActivity.kt
@@ -0,0 +1,13 @@
+package com.github.watabee.rakutenapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import dagger.android.AndroidInjection
+
+abstract class BaseActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        AndroidInjection.inject(this)
+        super.onCreate(savedInstanceState)
+    }
+}

16cd08746a91afc097e0f248b6a92f1d0ae84eaa
watabee <watabee.dev@gmail.com>
2019-10-22 13:45:12 +0900

    Bump agp version to 3.5.1

--- buildSrc/src/main/java/dependencies/dependencies.kt
+++ buildSrc/src/main/java/dependencies/dependencies.kt
@@ -19,7 +19,7 @@ object Versions {
 
 object Deps {
 
-    const val androidGradlePlugin = "com.android.tools.build:gradle:3.5.0"
+    const val androidGradlePlugin = "com.android.tools.build:gradle:3.5.1"
     const val androidJunit5GradlePlugin = "de.mannodermaus.gradle.plugins:android-junit5:1.5.1.0"
     const val spotlessGradlePlugin = "com.diffplug.spotless:spotless-plugin-gradle:3.24.2"
 

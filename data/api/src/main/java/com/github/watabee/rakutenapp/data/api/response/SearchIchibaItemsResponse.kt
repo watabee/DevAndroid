9bb5f8ed5bf0f42422df58590647400c4ed453e8
watabee <watabee.dev@gmail.com>
2019-11-04 16:47:06 +0900

    Tweak SearchIchibaItemsResponse

--- data/api/src/main/java/com/github/watabee/rakutenapp/data/api/response/SearchIchibaItemsResponse.kt
+++ data/api/src/main/java/com/github/watabee/rakutenapp/data/api/response/SearchIchibaItemsResponse.kt
@@ -10,5 +10,22 @@ data class SearchIchibaItemsResponse(
     val first: Int,
     val last: Int,
     val pageCount: Int,
-    @Json(name = "Items") val items: List<IchibaItem>
-)
+    @Json(name = "Items") val items: List<Item>
+) {
+    @JsonClass(generateAdapter = true)
+    data class Item(
+        val itemCaption: String,
+        val itemCode: String,
+        val itemName: String,
+        val itemPrice: Int,
+        val itemUrl: String,
+        val mediumImageUrls: List<String>,
+        val reviewAverage: String,
+        val reviewCount: Int,
+        val shopCode: String,
+        val shopName: String,
+        val shopUrl: String,
+        val smallImageUrls: List<String>,
+        val taxFlag: Int
+    )
+}

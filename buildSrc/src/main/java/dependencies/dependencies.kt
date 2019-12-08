7baf6a153235a31c1d13a9c101ed0833777fce69
watabee <watabee.dev@gmail.com>
2019-12-08 20:50:16 +0900

    Delete ndk version

--- buildSrc/src/main/java/dependencies/dependencies.kt
+++ buildSrc/src/main/java/dependencies/dependencies.kt
@@ -6,7 +6,6 @@ object Versions {
     const val androidTargetSdkVersion = 29
 
     const val buildToolsVersion = "29.0.2"
-    const val ndkVersion = "20.0.5594570"
 
     private const val versionMajor = 1
     private const val versionMinor = 0

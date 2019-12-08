78924ef832bf9402ed8541f2c21e822ee8d844f9
watabee <watabee.dev@gmail.com>
2019-12-08 21:20:16 +0900

    Set ndk version

--- buildSrc/src/main/java/dependencies/dependencies.kt
+++ buildSrc/src/main/java/dependencies/dependencies.kt
@@ -6,6 +6,7 @@ object Versions {
     const val androidTargetSdkVersion = 29
 
     const val buildToolsVersion = "29.0.2"
+    const val ndkVersion = "20.1.5948944"
 
     private const val versionMajor = 1
     private const val versionMinor = 0

89230c7e2c564e263cb189156294d45d44b6a14c
watabee <watabee.dev@gmail.com>
2019-12-21 17:49:58 +0900

    Kotlin Coroutines 1.3.3

--- buildSrc/src/main/java/dependencies/dependencies.kt
+++ buildSrc/src/main/java/dependencies/dependencies.kt
@@ -35,7 +35,7 @@ object Deps {
         const val reflect = "org.jetbrains.kotlin:kotlin-reflect:$version"
 
         object Coroutines {
-            private const val version = "1.3.2"
+            private const val version = "1.3.3"
             const val core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$version"
             const val android = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$version"
             const val test = "org.jetbrains.kotlinx:kotlinx-coroutines-test:$version"

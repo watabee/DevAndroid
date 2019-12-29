9891ca5b8d2470bde9cdb4202fd755fa56145fb5
watabee <watabee.dev@gmail.com>
2019-12-29 13:16:02 +0900

    Update top activity

--- ui/top/src/main/java/com/github/watabee/devtoapp/di/UiTopModule.kt
+++ ui/top/src/main/java/com/github/watabee/devtoapp/di/UiTopModule.kt
@@ -1,14 +1,8 @@
 package com.github.watabee.devtoapp.di
 
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModel
 import com.github.watabee.devtoapp.ui.top.TopActivity
-import com.github.watabee.devtoapp.ui.top.TopFragment
-import com.github.watabee.devtoapp.ui.top.TopViewModel
-import dagger.Binds
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
-import dagger.multibindings.IntoMap
 
 @Module
 abstract class UiTopModule {
@@ -16,14 +10,4 @@ abstract class UiTopModule {
     @ActivityScope
     @ContributesAndroidInjector
     internal abstract fun contributeActivityInjector(): TopActivity
-
-    @Binds
-    @IntoMap
-    @FragmentKey(TopFragment::class)
-    internal abstract fun bindFragment(fragment: TopFragment): Fragment
-
-    @Binds
-    @IntoMap
-    @ViewModelKey(TopViewModel::class)
-    internal abstract fun bindTopViewModel(instance: TopViewModel): ViewModel
 }

cbf4de04329746fb58a6bb9e267a6ba5770a5e0b
watabee <watabee.dev@gmail.com>
2019-12-29 11:22:28 +0900

    Show article tags

--- ui/articles/src/main/java/com/github/watabee/devtoapp/ui/articles/ArticleUiModel.kt
+++ ui/articles/src/main/java/com/github/watabee/devtoapp/ui/articles/ArticleUiModel.kt
@@ -2,13 +2,15 @@ package com.github.watabee.devtoapp.ui.articles
 
 import com.github.watabee.devtoapp.data.api.response.Article
 
+private const val MAX_TAGS_COUNT = 5
+
 internal data class ArticleUiModel(
     val id: Int,
     val title: String,
     val userImage: String,
     val username: String,
     val readablePublishDate: String,
-    val tagList: List<String>
+    val tagList: List<String?>
 ) {
     constructor(article: Article) : this(
         article.id,
@@ -16,6 +18,6 @@ internal data class ArticleUiModel(
         article.user.profileImage90,
         article.user.username,
         article.readablePublishDate,
-        article.tagList
+        List(MAX_TAGS_COUNT, article.tagList::getOrNull)
     )
 }
--- ui/common/src/main/java/com/github/watabee/devtoapp/ui/AppBindingAdapters.kt
+++ ui/common/src/main/java/com/github/watabee/devtoapp/ui/AppBindingAdapters.kt
@@ -1,8 +1,16 @@
 package com.github.watabee.devtoapp.ui
 
+import android.widget.TextView
 import androidx.annotation.ColorInt
+import androidx.core.view.isGone
 import androidx.databinding.BindingAdapter
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+
+@BindingAdapter("textOrGoneIfEmpty")
+fun textOrGoneIfEmpty(view: TextView, s: CharSequence?) {
+    view.text = s
+    view.isGone = s.isNullOrEmpty()
+}
 
 @BindingAdapter("colorSchemeColor")
 fun setColorSchemeColor(view: SwipeRefreshLayout, @ColorInt color: Int) {

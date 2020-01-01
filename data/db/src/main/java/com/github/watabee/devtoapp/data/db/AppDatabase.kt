6730d4c1733b17aeb2b9281538238685636df89e
watabee <watabee.dev@gmail.com>
2020-01-01 20:41:57 +0900

    Setup database and db module

--- data/db/src/main/java/com/github/watabee/devtoapp/data/db/AppDatabase.kt
+++ data/db/src/main/java/com/github/watabee/devtoapp/data/db/AppDatabase.kt
@@ -4,13 +4,19 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.github.watabee.devtoapp.data.db.daos.ArticleDao
+import com.github.watabee.devtoapp.data.db.entities.ArticleDetailEntity
 
 @Database(
-    entities = [],
+    entities = [ArticleDetailEntity::class],
     version = 1,
     exportSchema = true
 )
+@TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun articleDao(): ArticleDao
 
     companion object {
 

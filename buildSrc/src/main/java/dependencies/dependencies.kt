0cbc8e0129a8790ba10dcf13b327e24fdeb66d7a
watabee <watabee.dev@gmail.com>
2020-04-04 17:21:34 +0900

    Room 2.2.5

--- buildSrc/src/main/java/dependencies/dependencies.kt
+++ buildSrc/src/main/java/dependencies/dependencies.kt
@@ -90,7 +90,7 @@ object Deps {
         }
 
         object Room {
-            private const val version = "2.2.3"
+            private const val version = "2.2.5"
             const val runtime = "androidx.room:room-runtime:$version"
             const val compiler = "androidx.room:room-compiler:$version"
             const val ktx = "androidx.room:room-ktx:$version"

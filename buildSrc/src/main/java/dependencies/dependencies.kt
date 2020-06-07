f3591bb6ab8a547e321eb5a1494b8561d0b8d7dd
watabee <watabee.dev@gmail.com>
2020-06-07 05:03:21 +0000

    Upgraded dev.chrisbanes:insetter-ktx from 0.2.1 to 0.2.2

--- buildSrc/src/main/java/dependencies/dependencies.kt
+++ buildSrc/src/main/java/dependencies/dependencies.kt
@@ -134,7 +134,7 @@ object Deps {
     }
 
     object Insetter {
-        const val insetterKtx = "dev.chrisbanes:insetter-ktx:0.2.1"
+        const val insetterKtx = "dev.chrisbanes:insetter-ktx:0.2.2"
         const val insetterDbx = "dev.chrisbanes:insetter-dbx:0.2.1"
     }
 

e584f07e9f1f4ad73f55d649d94c2a6c1a71ca70
watabee <watabee.dev@gmail.com>
2020-06-07 03:33:42 +0000

    Upgraded com.google.truth:truth from 1.0 to 1.0.1

--- buildSrc/src/main/java/dependencies/dependencies.kt
+++ buildSrc/src/main/java/dependencies/dependencies.kt
@@ -153,7 +153,7 @@ object Deps {
     }
 
     const val junit = "junit:junit:4.13"
-    const val truth = "com.google.truth:truth:1.0"
+    const val truth = "com.google.truth:truth:1.0.1"
 
     object Spek2 {
         const val dslJvm = "org.spekframework.spek2:spek-dsl-jvm:2.0.7"

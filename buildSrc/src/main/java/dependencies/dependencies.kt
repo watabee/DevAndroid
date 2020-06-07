1f09bd8a0c6c7d7c806c55536c1e838b64985698
watabee <watabee.dev@gmail.com>
2020-06-07 05:03:52 +0000

    Upgraded de.mannodermaus.gradle.plugins:android-junit5 from 1.5.1.0 to 1.6.2.0

--- buildSrc/src/main/java/dependencies/dependencies.kt
+++ buildSrc/src/main/java/dependencies/dependencies.kt
@@ -22,7 +22,7 @@ object Deps {
 
     const val androidGradlePlugin = "com.android.tools.build:gradle:4.0.0"
     const val googleServicesGradlePlugin = "com.google.gms:google-services:4.3.3"
-    const val androidJunit5GradlePlugin = "de.mannodermaus.gradle.plugins:android-junit5:1.5.1.0"
+    const val androidJunit5GradlePlugin = "de.mannodermaus.gradle.plugins:android-junit5:1.6.2.0"
     const val ktlintGradlePlugin = "org.jlleitschuh.gradle:ktlint-gradle:9.2.1"
     const val detektGradlePlugin = "io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.7.1"
     // https://arunkumar9t2.github.io/scabbard/

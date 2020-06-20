9f5ea10b4169d6a748b29680275323fa877f70d2
watabee <watabee.dev@gmail.com>
2020-06-20 13:14:46 +0900

    Create TimberInitializer using App Startup

--- base-android/src/main/java/com/github/watabee/devtoapp/di/BaseModule.kt
+++ base-android/src/main/java/com/github/watabee/devtoapp/di/BaseModule.kt
@@ -1,9 +1,6 @@
 package com.github.watabee.devtoapp.di
 
-import com.github.watabee.devtoapp.util.AppLogger
 import com.github.watabee.devtoapp.util.CoroutineDispatchers
-import com.github.watabee.devtoapp.util.Logger
-import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -13,20 +10,14 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(ApplicationComponent::class)
-abstract class BaseModule {
+object BaseModule {
 
+    @Provides
     @Singleton
-    @Binds
-    internal abstract fun bindLogger(instance: AppLogger): Logger
-
-    companion object {
-        @Provides
-        @Singleton
-        fun provideCoroutineDispatchers(): CoroutineDispatchers =
-            CoroutineDispatchers(
-                main = Dispatchers.Main,
-                io = Dispatchers.IO,
-                computation = Dispatchers.Default
-            )
-    }
+    fun provideCoroutineDispatchers(): CoroutineDispatchers =
+        CoroutineDispatchers(
+            main = Dispatchers.Main,
+            io = Dispatchers.IO,
+            computation = Dispatchers.Default
+        )
 }

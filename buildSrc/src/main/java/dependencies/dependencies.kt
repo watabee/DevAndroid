df7a2412f5f2344de1ad4eac6541199572e296f8
watabee <watabee.dev@gmail.com>
2020-08-05 20:10:13 +0000

    Upgraded androidx.appcompat:appcompat from 1.1.0 to 1.2.0

--- buildSrc/src/main/java/dependencies/dependencies.kt
+++ buildSrc/src/main/java/dependencies/dependencies.kt
@@ -48,7 +48,7 @@ object Deps {
     }
 
     object AndroidX {
-        const val appcompat = "androidx.appcompat:appcompat:1.1.0"
+        const val appcompat = "androidx.appcompat:appcompat:1.2.0"
         const val recyclerview = "androidx.recyclerview:recyclerview:1.2.0-alpha04"
         const val swiperefresh = "androidx.swiperefreshlayout:swiperefreshlayout:1.1.0"
 

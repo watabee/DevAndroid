cf9f2404b9646d3ac9dd44b5b0edacb0e3fd3405
watabee <watabee.dev@gmail.com>
2020-08-26 20:11:41 +0000

    Upgraded androidx.test:rules from 1.2.0 to 1.3.0

--- buildSrc/src/main/java/dependencies/dependencies.kt
+++ buildSrc/src/main/java/dependencies/dependencies.kt
@@ -58,7 +58,7 @@ object Deps {
         object Test {
             const val core = "androidx.test:core:1.3.0"
             const val runner = "androidx.test:runner:1.2.0"
-            const val rules = "androidx.test:rules:1.2.0"
+            const val rules = "androidx.test:rules:1.3.0"
 
             const val espressoCore = "androidx.test.espresso:espresso-core:3.2.0"
         }

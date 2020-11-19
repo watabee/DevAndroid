4a20135efadbd036db7f32dc82ba38dc003cdfd4
watabee <watabee.dev@gmail.com>
2020-11-19 20:12:00 +0000

    Upgraded org.jetbrains.kotlin:kotlin-gradle-plugin from 1.4.10 to 1.4.20

--- buildSrc/src/main/java/dependencies/dependencies.kt
+++ buildSrc/src/main/java/dependencies/dependencies.kt
@@ -33,7 +33,7 @@ object Deps {
 
     object Kotlin {
         const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.4.20"
-        const val gradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.10"
+        const val gradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.20"
         const val reflect = "org.jetbrains.kotlin:kotlin-reflect:1.4.10"
 
         object Coroutines {

e0737ced97e0960ee2143092f095c9a7d2c6e5e8
watabee <watabee.dev@gmail.com>
2021-02-10 20:12:35 +0000

    Upgraded androidx.activity:activity-ktx from 1.2.0-rc01 to 1.2.0

--- buildSrc/src/main/java/dependencies/dependencies.kt
+++ buildSrc/src/main/java/dependencies/dependencies.kt
@@ -52,7 +52,7 @@ object Deps {
         const val recyclerview = "androidx.recyclerview:recyclerview:1.2.0-alpha05"
         const val swiperefresh = "androidx.swiperefreshlayout:swiperefreshlayout:1.1.0"
 
-        const val activityKtx = "androidx.activity:activity-ktx:1.2.0-rc01"
+        const val activityKtx = "androidx.activity:activity-ktx:1.2.0"
         const val fragmentKtx = "androidx.fragment:fragment-ktx:1.3.0-rc01"
 
         object Test {

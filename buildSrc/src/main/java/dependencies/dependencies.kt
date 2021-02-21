9cb6276014e0d4c9ebfe14b09d6bf69e591ec9d5
watabee <watabee.dev@gmail.com>
2021-02-21 10:12:59 +0900

    Paging 3.0.0-beta01

--- buildSrc/src/main/java/dependencies/dependencies.kt
+++ buildSrc/src/main/java/dependencies/dependencies.kt
@@ -94,7 +94,7 @@ object Deps {
         }
 
         object Paging {
-            const val runtime = "androidx.paging:paging-runtime:3.0.0-alpha11"
+            const val runtime = "androidx.paging:paging-runtime:3.0.0-beta01"
         }
     }
 

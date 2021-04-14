e550a2958a080e264bb9a488dd54f8997e4e3829
watabee <watabee.dev@gmail.com>
2021-04-14 20:07:33 +0000

    Upgraded com.facebook.flipper:flipper from 0.84.0 to 0.85.0

--- buildSrc/src/main/java/dependencies/dependencies.kt
+++ buildSrc/src/main/java/dependencies/dependencies.kt
@@ -149,7 +149,7 @@ object Deps {
     }
 
     object Flipper {
-        const val flipper = "com.facebook.flipper:flipper:0.84.0"
+        const val flipper = "com.facebook.flipper:flipper:0.85.0"
         const val networkPlugin = "com.facebook.flipper:flipper-network-plugin:0.84.0"
 
         const val flipperNoOp = "com.facebook.flipper:flipper-noop:0.84.0"

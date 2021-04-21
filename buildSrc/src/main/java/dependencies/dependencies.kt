c294ea44f9693ea69017bed7da211d78ef94e63d
watabee <watabee.dev@gmail.com>
2021-04-21 20:07:12 +0000

    Upgraded androidx.room:room-ktx from 2.3.0-rc01 to 2.3.0

--- buildSrc/src/main/java/dependencies/dependencies.kt
+++ buildSrc/src/main/java/dependencies/dependencies.kt
@@ -83,7 +83,7 @@ object Deps {
         object Room {
             const val runtime = "androidx.room:room-runtime:2.3.0"
             const val compiler = "androidx.room:room-compiler:2.3.0"
-            const val ktx = "androidx.room:room-ktx:2.3.0-rc01"
+            const val ktx = "androidx.room:room-ktx:2.3.0"
             const val testing = "androidx.room:room-testing:2.3.0-rc01"
         }
 

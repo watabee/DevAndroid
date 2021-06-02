81ffc9e3b62086d1ddd57a75345f53c20cd6d597
watabee <watabee.dev@gmail.com>
2021-06-02 20:48:55 +0000

    Upgraded com.facebook.flipper:flipper-noop from 0.91.2 to 0.92.0

--- buildSrc/src/main/java/dependencies/dependencies.kt
+++ buildSrc/src/main/java/dependencies/dependencies.kt
@@ -146,7 +146,7 @@ object Deps {
         const val flipper = "com.facebook.flipper:flipper:0.92.0"
         const val networkPlugin = "com.facebook.flipper:flipper-network-plugin:0.92.0"
 
-        const val flipperNoOp = "com.facebook.flipper:flipper-noop:0.91.2"
+        const val flipperNoOp = "com.facebook.flipper:flipper-noop:0.92.0"
     }
 
     const val soLoader = "com.facebook.soloader:soloader:0.9.0"

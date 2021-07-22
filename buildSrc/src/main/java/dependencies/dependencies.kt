8b9035a0d7994a910ac28cf71b9cc2c4d196c5bf
watabee <watabee.dev@gmail.com>
2021-07-22 20:05:58 +0000

    Upgraded com.google.dagger:hilt-compiler from 2.37 to 2.38

--- buildSrc/src/main/java/dependencies/dependencies.kt
+++ buildSrc/src/main/java/dependencies/dependencies.kt
@@ -100,7 +100,7 @@ object Deps {
     object Hilt {
         const val gradlePlugin = "com.google.dagger:hilt-android-gradle-plugin:2.37"
         const val android = "com.google.dagger:hilt-android:2.37"
-        const val compiler = "com.google.dagger:hilt-compiler:2.37"
+        const val compiler = "com.google.dagger:hilt-compiler:2.38"
     }
 
     object Glide {

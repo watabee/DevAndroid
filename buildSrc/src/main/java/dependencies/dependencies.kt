143056e794ac68870f968e2ae1784361ee4997af
watabee <watabee.dev@gmail.com>
2021-07-28 20:06:16 +0000

    Upgraded androidx.appcompat:appcompat from 1.3.0 to 1.3.1

--- buildSrc/src/main/java/dependencies/dependencies.kt
+++ buildSrc/src/main/java/dependencies/dependencies.kt
@@ -47,7 +47,7 @@ object Deps {
     }
 
     object AndroidX {
-        const val appcompat = "androidx.appcompat:appcompat:1.3.0"
+        const val appcompat = "androidx.appcompat:appcompat:1.3.1"
         const val recyclerview = "androidx.recyclerview:recyclerview:1.2.1"
         const val swiperefresh = "androidx.swiperefreshlayout:swiperefreshlayout:1.1.0"
 

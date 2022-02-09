4a5c13d9647aa5adf28cef2c9f066471e3192c03
watabee <watabee.dev@gmail.com>
2022-02-09 20:04:58 +0000

    Upgraded androidx.paging:paging-runtime from 3.0.1 to 3.1.0

--- buildSrc/src/main/java/dependencies/dependencies.kt
+++ buildSrc/src/main/java/dependencies/dependencies.kt
@@ -93,7 +93,7 @@ object Deps {
         }
 
         object Paging {
-            const val runtime = "androidx.paging:paging-runtime:3.0.1"
+            const val runtime = "androidx.paging:paging-runtime:3.1.0"
         }
     }
 

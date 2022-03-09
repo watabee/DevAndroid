bdfee1e3c842005cba7032e67c6a721a8b8d6b1a
watabee <watabee.dev@gmail.com>
2022-03-09 20:04:47 +0000

    Upgraded com.squareup.moshi:moshi-adapters from 1.12.0 to 1.13.0

--- buildSrc/src/main/java/dependencies/dependencies.kt
+++ buildSrc/src/main/java/dependencies/dependencies.kt
@@ -123,7 +123,7 @@ object Deps {
     object Moshi {
         const val kotlin = "com.squareup.moshi:moshi-kotlin:1.12.0"
         const val codegen = "com.squareup.moshi:moshi-kotlin-codegen:1.12.0"
-        const val adapters = "com.squareup.moshi:moshi-adapters:1.12.0"
+        const val adapters = "com.squareup.moshi:moshi-adapters:1.13.0"
     }
 
     object Insetter {

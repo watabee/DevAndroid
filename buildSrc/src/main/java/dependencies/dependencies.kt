e5bad18157e5db910fd09d736626311d2513a590
watabee <watabee.dev@gmail.com>
2022-10-20 20:05:42 +0000

    Upgraded com.squareup.okhttp3:logging-interceptor from 4.9.3 to 4.10.0

--- buildSrc/src/main/java/dependencies/dependencies.kt
+++ buildSrc/src/main/java/dependencies/dependencies.kt
@@ -111,7 +111,7 @@ object Deps {
 
     object OkHttp {
         const val okhttp = "com.squareup.okhttp3:okhttp:4.10.0"
-        const val loggingInterceptor = "com.squareup.okhttp3:logging-interceptor:4.9.3"
+        const val loggingInterceptor = "com.squareup.okhttp3:logging-interceptor:4.10.0"
     }
 
     object Retrofit {

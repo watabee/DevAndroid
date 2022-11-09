2aaf315435316cf6340e80c748e006e94d24d5c3
watabee <watabee.dev@gmail.com>
2022-11-09 20:05:31 +0000

    Upgraded org.jetbrains.kotlinx:kotlinx-coroutines-android from 1.5.2 to 1.6.4

--- buildSrc/src/main/java/dependencies/dependencies.kt
+++ buildSrc/src/main/java/dependencies/dependencies.kt
@@ -37,7 +37,7 @@ object Deps {
 
         object Coroutines {
             const val core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4"
-            const val android = "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2"
+            const val android = "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4"
             const val test = "org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4"
         }
     }
